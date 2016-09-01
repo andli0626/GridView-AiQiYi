d114efa33461517102695fe10093c2570b266e37
李林 <ww1095@163.com>
2016-09-01 15:58:45 +0800

    1

---
+++ UI_GridView/src/com/andli/gridview/TestView.java
@@ -0,0 +1,13 @@
+package com.andli.gridview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestView extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
